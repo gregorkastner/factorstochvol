5357c93f909f5fc73504bbd5f23e3b247e633161
Gregor Kastner <gregor.kastner@wu.ac.at>
2019-02-01 15:14:41 +0100

    getting ready for factorstochvol 0.9: updated references small fixes

---
+++ src/register.cpp
@@ -0,0 +1,17 @@
+#include "sampler.h"
+#include "predict.h"
+#include "dmvnorm.h"
+
+using namespace Rcpp;
+
+static const R_CallMethodDef CallEntries[] = {
+    {"sampler", (DL_FUNC) &sampler, 31},
+    {"predict", (DL_FUNC) &predict, 3},
+    {"dmvnorm", (DL_FUNC) &dmvnorm, 4},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_factorstochvol(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
