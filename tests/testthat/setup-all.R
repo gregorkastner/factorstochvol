176546a96f989afa4cd0fc135e5b2bf1281a43e7
Darjus Hosszejni <darjus.hosszejni@wu.ac.at>
2019-07-10 17:00:25 +0200

    updated tests

--- tests/testthat/setup-all.R
+++ tests/testthat/setup-all.R
@@ -9,24 +9,32 @@ factors_values <- c(0, 1, 3)
 restrict_mat <- matrix(FALSE, nrow = NCOL(y), ncol = max(factors_values))
 restrict_mat[1, 1] <- TRUE  # restrict the upper left element to zero
 restrict_values <- list("upper", "auto", restrict_mat)
-priorfacloadtype_values <- c("normal", "rowwiseng", "colwiseng")
-#priorhomoskedastic <- matrix(c(.1, .5), nrow = NCOL(y)+max(factors_values),
-#                             ncol = 2, byrow = TRUE)
-#heteroskedastic_values <- list(TRUE, c(FALSE, FALSE))
+priorfacloadtype_values <- c("normal", "rowwiseng", "colwiseng", "dl")
+priorhomoskedastic <- matrix(c(1.1, 1.1), nrow = NCOL(y)+max(factors_values),
+                             ncol = 2, byrow = TRUE)
+heteroskedastic_values <- list(TRUE, c(FALSE, FALSE))
 
 res <- list(fsvsample(y, draws = draws, burnin = burnin, quiet = TRUE))
 for (th in thin_values) {
   for (pflt in priorfacloadtype_values) {
-    for (fs in factors_values) {
-      res <- c(res, list(fsvsample(y, draws = draws, burnin = burnin, quiet = TRUE,
-                                   factors = fs, thin = th, priorfacloadtype = pflt,
-                                   restrict = "none",
-                                   runningstore = if (fs == 0) 1 else 6)))
-      if (fs > 1) {
-        for (rst in restrict_values) {
-          res <- c(res, list(fsvsample(y, draws = draws, burnin = burnin, quiet = TRUE,
-                                       factors = fs, thin = th, priorfacloadtype = pflt,
-                                       restrict = rst)))
+    for (hsk in heteroskedastic_values) {
+      for (fs in factors_values) {
+        res <- c(res, list(fsvsample(y, draws = draws, burnin = burnin, quiet = TRUE,
+                                     factors = fs, thin = th, priorfacloadtype = pflt,
+                                     restrict = "none",
+                                     heteroskedastic = hsk,
+                                     priorhomoskedastic = if (!isTRUE(hsk)) priorhomoskedastic[seq_len(NCOL(y)+fs), ] else NA,
+                                     interweaving = if (!isTRUE(hsk)) 0 else 4,
+                                     runningstore = if (fs == 0) 1 else 6)))
+        if (fs > 1) {
+          for (rst in restrict_values) {
+            res <- c(res, list(fsvsample(y, draws = draws, burnin = burnin, quiet = TRUE,
+                                         factors = fs, thin = th, priorfacloadtype = pflt,
+                                         heteroskedastic = hsk,
+                                         priorhomoskedastic = if (!isTRUE(hsk)) priorhomoskedastic[seq_len(NCOL(y)+fs), ] else NA,
+                                         interweaving = if (!isTRUE(hsk)) 0 else 4,
+                                         restrict = rst)))
+          }
         }
       }
     }
--- tests/testthat/test-wrappers.R
+++ tests/testthat/test-wrappers.R
@@ -5,18 +5,26 @@ test_that("fsvsample works", {
     expect_is("fsvdraws")
   for (th in thin_values) {
     for (pflt in priorfacloadtype_values) {
-      for (fs in factors_values) {
-        expect_warning(fsvsample(y, draws = draws, burnin = burnin, quiet = TRUE,
-                                     factors = fs, thin = th, priorfacloadtype = pflt,
-                                     restrict = "none",
-                                     runningstore = if (fs == 0) 1 else 6), NA) %>%
-          expect_is("fsvdraws")
-        if (fs > 1) {
-          for (rst in restrict_values) {
-            expect_warning(fsvsample(y, draws = draws, burnin = burnin, quiet = TRUE,
-                                         factors = fs, thin = th, priorfacloadtype = pflt,
-                                         restrict = rst), NA) %>%
-              expect_is("fsvdraws")
+      for (hsk in heteroskedastic_values) {
+        for (fs in factors_values) {
+          expect_warning(fsvsample(y, draws = draws, burnin = burnin, quiet = TRUE,
+                                   factors = fs, thin = th, priorfacloadtype = pflt,
+                                   restrict = "none",
+                                   heteroskedastic = hsk,
+                                   priorhomoskedastic = if (!isTRUE(hsk)) priorhomoskedastic[seq_len(NCOL(y)+fs), ] else NA,
+                                   interweaving = if (!isTRUE(hsk)) 0 else 4,
+                                   runningstore = if (fs == 0) 1 else 6), NA) %>%
+            expect_is("fsvdraws")
+          if (fs > 1) {
+            for (rst in restrict_values) {
+              expect_warning(fsvsample(y, draws = draws, burnin = burnin, quiet = TRUE,
+                                       factors = fs, thin = th, priorfacloadtype = pflt,
+                                       heteroskedastic = hsk,
+                                       priorhomoskedastic = if (!isTRUE(hsk)) priorhomoskedastic[seq_len(NCOL(y)+fs), ] else NA,
+                                       interweaving = if (!isTRUE(hsk)) 0 else 4,
+                                       restrict = rst), NA) %>%
+                expect_is("fsvdraws")
+            }
           }
         }
       }
