85d58f7b1568ae67bab2bcddfa1214ce9c6b0b11
Darjus Hosszejni <darjus.hosszejni@wu.ac.at>
2021-11-25 15:17:01 +0100

    test suite for the plotting functions

---
+++ tests/testthat/test-plotting.R
@@ -0,0 +1,23 @@
+test_that("plots execute error-free", {
+  data("exrates", package = "stochvol")
+  m <- 6
+  n <- 50
+  y <- 100 * logret(tail(exrates[, seq_len(m)], n + 1))
+  res <- fsvsample(y, factors = 2, draws = 100, zeromean = FALSE,
+                   thin = 1, quiet = TRUE)
+  expect_invisible(comtimeplot(res))
+  expect_invisible(voltimeplot(res))
+  expect_invisible(corimageplot(res, plottype = "corrplot"))
+  expect_invisible(corimageplot(res, plottype = "imageplot"))
+  expect_invisible(cortimeplot(res, series = 2))
+  expect_invisible(covtimeplot(res, series = 2))
+  expect_invisible(facloadpairplot(res))
+  expect_invisible(facloadcredplot(res))
+  expect_invisible(facloadpointplot(res))
+  expect_invisible(logvartimeplot(res))
+  expect_invisible(paratraceplot(res))
+  expect_invisible(facloadtraceplot(res))
+  expect_invisible(facloaddensplot(res))
+  expect_invisible(corplot(res))
+  expect_invisible(evdiag(res))
+})
